e909f94ac93e850093ebc93371aeb56928f1a82b
Kitt Cheung <kitt.cheung@live.se>
2022-01-20 20:41:31 +0100

    FEAT: Updating Login template

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,18 @@
 <template>
-  <h1>Login to get started</h1>
-  <form>
-      <fieldset>
-          <label for="username" aria-label="Username">Username</label>
-          <input type="text" id="username" placeholder="E.g., john.doe@email.com" />
-      </fieldset>
+    <main class="container mx-auto px-4">
+        <h1 class="mb-3 text-2xl">Login to get started</h1>
+        <form>
+            <fieldset class="mb-3">
+            <label for="username" aria-label="Username" class="block">Username</label>
+            <input type="text" id="username" placeholder="E.g., john.doe@email.com" class="border border-slate-300" />
+            </fieldset>
 
-      <fieldset>
-          <label for="password" aria-label="password">password</label>
-          <input type="password" id="password" placeholder="*****" />
-      </fieldset>
+            <fieldset class="mb-3">
+            <label for="password" aria-label="password" class="block">password </label>
+            <input type="password" id="password" placeholder="*****" class="border border-slate-300" />
+            </fieldset>
 
-      <button type="submit" >Register</button>
-  </form>
+            <button type="submit" class="bg-indigo-500 text-white p-3 rounded">Register</button>
+        </form>
+    </main>
 </template>
