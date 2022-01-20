f6bc36d9945ea2c5c1dcd0df82bb12577cb68850
Kitt Cheung <kitt.cheung@live.se>
2022-01-20 20:12:03 +0100

    FEAT: adding Login page features

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Login to get started</h1>
+  <form>
+      <fieldset>
+          <label for="username" aria-label="Username">Username</label>
+          <input type="text" id="username" placeholder="E.g., john.doe@email.com" />
+      </fieldset>
+
+      <fieldset>
+          <label for="password" aria-label="password">password</label>
+          <input type="password" id="password" placeholder="*****" />
+      </fieldset>
+
+      <button type="submit" >Register</button>
+  </form>
+</template>
