9bb3e12e3c5909470c5475f2218ddaeee4b49397
Kitt Cheung <kitt.cheung@se.experis.com>
2022-01-20 17:37:17 +0100

    First commit: Startup test project with Vue

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore } from "vuex"
+
+export default createStore({
+
+    //state shared through out the application
+    state: {
+
+    },
+    // chanign the state
+    mutations: {
+
+    },
+    //communication ex: HTTP requests
+    actions: {
+
+    }
+})
