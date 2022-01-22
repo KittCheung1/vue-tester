a19d6518dd3996c2e43584b0227929cf774d259a
Kitt Cheung <kitt.cheung@live.se>
2022-01-22 15:49:03 +0100

    Feat:(user.js) login user not working

--- src/api/users.js
+++ src/api/users.js
@@ -1,36 +1,40 @@
 import { BASE_USER_URL } from "./index";
 const apiKey = '334H7SGhAEiIPqPfCg+pfA=='
 
-const getUser = async function() {
+// GET --------------------------------------------------------------------------------------------
+// export async function apiGetUser(username) {
+//     const username = 'dewaldes'
 
-}
 
-
-
-//  POST-------------------------------------------------------------------
-// fetch(`${BASE_USER_URL}`, {
-//         method: 'POST',
-//         headers: {
-//             'X-API-Key': apiKey,
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             username: 'the-trivia-master',
-//             highScore: 0
+//     fetch(`${BASE_USER_URL}?username=${username}`)
+//         .then(response => response.json())
+//         .then(results => {
+//             // results will be an array of users that match the username of mega-mind.
 //         })
-//     })
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Could not create new user')
-//         }
-//         return response.json()
-//     })
-//     .then(newUser => {
-//         // newUser is the new user with an id
-//     })
-//     .catch(error => {})
+//         .catch(error => {})
+// }
+export async function apiUserLoginRegister(action = "login", username) {
+    try {
+        const config = {
 
+            method: "POST",
+            headers: {
+                'X-API-Key': apiKey,
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
 
+                username,
+            })
+        }
+
+        const response = await fetch(`${BASE_USER_URL}/${action}`, config);
+        const { data } = await response.json();
+        return [null, data];
+    } catch (error) {
+        return [error.message, null];
+    }
+}
 
 
 
@@ -46,16 +50,15 @@ export async function apiUserRegister(username) {
                 "content-type": "application/json"
             },
             body: JSON.stringify({
-                user: {
-                    username
-                }
+
+                username,
             })
         }
 
-        const response = await fetch(`${BASE_USER_URL}?username=${username}`, config);
-        const { success, data } = await response.json();
-        return [null, data]
+        const response = await fetch(`${BASE_USER_URL}`, config);
+        const { data } = await response.json();
+        return [null, data];
     } catch (error) {
-        return [error.message, null]
+        return [error.message, null];
     }
 }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,25 +2,36 @@
 import { ref } from "vue";
 import { reactive } from "vue";
 import { BASE_USER_URL } from "../api";
-import { apiUserRegister } from "../api/users";
+import { apiUserLoginRegister } from "../api/users";
 
 const username = ref("");
-const password = ref("");
-const users = reactive([]); 
+const displayError = ref("");
 
-const onSubmit = async ()=>{
-  const [error, user] = await apiUserRegister(username.value);
-//   console.log("ERR", error);
-//   console.log("user" , user);
+// const password = ref("");
+// const users = reactive([]); 
+
+const onRegisterClick = async () => {
+  const [error, user] = await apiUserLoginRegister("register",username.value);
+  if (error !== null) {
+    displayError.value = error;
+  } else console.log("You Passed!");
 }
 
+
+
+const onLoginClick = async() => {
+  const [error, user] = await apiUserLoginRegister("login",username.value);
+  if (error !== null) {
+    displayError.value = error;
+  } else console.log("You Passed!");
+}
 // const apiURL = BASE_USER_URL;
 // const username2 = 'dewaldes'
 // users = async function(){
 //  await fetch(`${apiURL}/trivia?username=${username2}`)
 //       .then(response => response.json())
 //       .then(results => {
-//           // results will be an array of users that match the username of mega-mind.
+//           // results will be an array of users that match the username of dewaldes.
 //       })
 //       .catch(error => {
 //       })
@@ -33,12 +44,10 @@ const onSubmit = async ()=>{
 <template>
   <main class="container mx-auto px-4">
     <h1 class="mb-3 text-2xl">Login to get started</h1>
-    
-    <form @submit.prevent="onSubmit">
+
+    <form >
       <fieldset class="mb-3">
-        <label for="username" aria-label="Username" class="block"
-          >Username</label
-        >
+        <label for="username" aria-label="Username" class="block">Username</label>
         <input
           type="text"
           id="username"
@@ -59,12 +68,11 @@ const onSubmit = async ()=>{
           class="border border-slate-300"
           v-model="password"
         />
-      </fieldset> -->
-
-      <button type="submit" class="bg-indigo-500 text-white p-3 rounded">
-        Register
-      </button>
+      </fieldset>-->
+      <div class="grid gap-2 grid-cols-2">
+        <button @click="onRegisterClick" type="button" class="bg-indigo-500 text-white p-3 rounded">Register</button>
+        <button @click="onLoginClick" type="button" class="bg-yellow-500 text-white p-3 rounded">Login</button>
+      </div>
     </form>
-
   </main>
 </template>
